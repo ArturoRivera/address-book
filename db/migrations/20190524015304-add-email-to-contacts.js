d257df1db4036fc842b72a7e7218f1c0c2bfbe0c
Arturo Rivera <a.rivera13@hotmail.com>
2019-05-28 15:27:24 -0700

    sequelizer error

--- db/migrations/20190524015304-add-email-to-contacts.js
+++ db/migrations/20190524015304-add-email-to-contacts.js
@@ -9,6 +9,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn("Contacts", "Email")
+    return queryInterface.removeColumn("Contacts", "Email");
   }
 };
